fb18b4d86d5d37f77b91aabddff18aea26a345c1
justtowelie <danielleggett1@gmail.com>
2023-01-31 09:34:02 +0000

    fixed import issues

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,30 +1,65 @@
-import java.util.regex.Pattern;
-import com.microsoft.playwright.*;
-import com.microsoft.playwright.options.AriaRole;
+package test;
 
-import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.*;
 
-public class App {
-    public static void main(String[] args) {
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch();
-            Page page = browser.newPage();
-            page.navigate("http://playwright.dev");
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-            // Expect a title "to contain" a substring.
-            assertThat(page).hasTitle(Pattern.compile("Playwright"));
+public class BaseTest {
+    // Shared between all tests in this class.
+    static Playwright playwright;
+    static Browser browser;
 
-            // create a locator
-            Locator getStarted = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Get Started"));
+    // New instance for each test method.
+    BrowserContext context;
+    Page page;
 
-            // Expect an attribute "to be strictly equal" to the value.
-            assertThat(getStarted).hasAttribute("href", "/docs/intro");
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
 
-            // Click the get started link.
-            getStarted.click();
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
 
-            // Expects the URL to contain intro.
-            assertThat(page).hasURL(Pattern.compile(".*intro"));
-        }
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+
+    @Test
+    void shouldClickButton() {
+        page.navigate("data:text/html,<script>var result;</script><button onclick='result=\"Clicked\"'>Go</button>");
+        page.locator("button").click();
+        assertEquals("Clicked", page.evaluate("result"));
+    }
+
+    @Test
+    void shouldCheckTheBox() {
+        page.setContent("<input id='checkbox' type='checkbox'></input>");
+        page.locator("input").check();
+        assertTrue((Boolean) page.evaluate("() => window['checkbox'].checked"));
+    }
+
+    @Test
+    void shouldSearchWiki() {
+        page.navigate("https://www.wikipedia.org/");
+        page.locator("input[name=\"search\"]").click();
+        page.locator("input[name=\"search\"]").fill("playwright");
+        page.locator("input[name=\"search\"]").press("Enter");
+        assertEquals("https://en.wikipedia.org/wiki/Playwright", page.url());
     }
 }
